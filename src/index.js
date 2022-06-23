af4b4b11eef06aa2c0502d918f6e092a29e9eb11
guschins <guschins@gmail.com>
2022-06-24 00:23:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numberToWords = require('number-to-words');
+    return numberToWords.toWords(number).split('-').join(' ');
 }
+
