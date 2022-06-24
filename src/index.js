061a1150d3ba38e3385a9e8438ec2fa8f7e4d1bf
guschins <guschins@gmail.com>
2022-06-24 10:47:06 +0300

    fix: add code without npm module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 module.exports = function toReadable(number) {
-    const numWords = require('num-words');
-    return numWords(number).split(' and ').join(' ');
+    let oneToNineteen = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ',
+        'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '
+    ];
+    let twentyToNinety = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let num = ('000' + number).slice(-3).match(/^(\d{1})(\d{2})$/);
+    if (number === 0) return 'zero'
+
+    let numberText = num[1] != 0 ? (oneToNineteen[Number(num[1])] || `${twentyToNinety[num[1][0]]} ${oneToNineteen[num[1][1]]}`) + 'hundred ' : '';
+
+    numberText += num[2] != 0 ? (oneToNineteen[Number(num[2])] || `${twentyToNinety[num[2][0]]} ${oneToNineteen[num[2][1]]}`)  : '';
+
+    return numberText.trim();
 }
-
-
-
-
-
-
-
-
-    // const numberToWords = require('number-to-words');
-    // console.log(numberToWords.toWords(number).split('-').join(' '));
-    // return numberToWords.toWords(number).split('-').join(' ');
-
-    // module.exports(265)
-
-
-    // const { ToWords } = require('to-words');
-    // const toWords = new ToWords();
-    // return toWords.convert(number).toLowerCase();
